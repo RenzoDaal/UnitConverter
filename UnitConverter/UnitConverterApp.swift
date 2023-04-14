f1ba1fc5315e26fe4b1a46070f846bfbf262503f
RenzoDaal <renzo20daal02@gmail.com>
2023-04-14 14:46:11 +0200

    Initial Commit

---
+++ UnitConverter/UnitConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConverterApp.swift
+//  UnitConverter
+//
+//  Created by Renzo Daal on 14/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct UnitConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
